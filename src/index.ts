bf342c0de1a9d0fc0920dc3e8ee08311f06ce859
AveryOn <lnkonion2@gmail.com>
2025-05-17 22:28:13 +0500

    minor refoactor

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,27 @@
 import { exec } from 'child_process';
 import fs from 'fs'
 
-function record(duration = 5): Promise<void> {
+function record(duration = 5): Promise<boolean> {
 return new Promise((resolve, reject) => {
-    console.log('🎙 Запись началась...');
     exec(`arecord -f cd -c 1 -r 16000 -d ${duration} input.wav`, (err) => {
         if (err) {
-            console.error('❌ Ошибка записи:', err);
             return reject(err); 
         }
-        console.log('🎙 Запись завершена');
-        resolve();
+        resolve(true);
     });
     });
 }
 
-function transcribe() {
+function transcribe(): Promise<string> {
   return new Promise((resolve, reject) => {
     /* 
       Whisper под капотом поздает одноименный файл input.txt. (на основе input.wav)
       input.txt лежит текст извлеченный с аудио
     */
-    exec('whisper input.wav --language Russian --fp16 False --model base', (error, stdout, stderr) => {
-      if (error) return reject(stderr);
-      console.log('📝 Распознавание завершено!_', stdout);
-      /* 
-        Тут важно выкинуть stdout наружу.
-        Это необходимо для голосового ассистента, он перехватывает stdout с этого процесса
-      */
-      console.log(stdout)
+    exec('whisper input.wav --language Russian --fp16 False --model base', (error, _, stderr) => {
+      if (error) 
+        return reject(stderr);
+
       const text = fs.readFileSync('./input.txt', 'utf-8');
       resolve(text.trim());
     });
@@ -39,8 +32,12 @@ function transcribe() {
 
 async function main() {
   await record(15);
-  const text = await transcribe();
-  console.log('📣 Ты сказал:', text);
+  const stdout: string = await transcribe();
+  /* 
+    Тут важно выкинуть stdout наружу через .log()
+    Это необходимо для голосового ассистента, он перехватывает stdout с этого процесса
+  */
+  console.log(stdout)
 }
 
 main();
